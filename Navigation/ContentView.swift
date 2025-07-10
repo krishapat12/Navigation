f776f6448ab04fb64bb0a4ce4aeb34c49f50b6b8
Krisha Patel <patel.krisha.edu@gmail.com>
2025-07-10 09:50:09 -0500

    Added the nav link and stage

--- Navigation/ContentView.swift
+++ Navigation/ContentView.swift
@@ -9,13 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                Text("This is the root view! 🌳")
+                NavigationLink(destination: Text("Congrats, you've arrived at the second view! 🎉")) {
+                    Text("Click Me!")
+                }
+                
+            }
+            .padding()
         }
-        .padding()
+
+        
     }
 }
 
